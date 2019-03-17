a76f2957c520fde861ba051b1268d66efea48314
Roman Karpov <roman.karpov@gmail.com>
2019-03-17 22:34:00 +0300

    mouse events in overlay canvas

--- chart.js
+++ chart.js
@@ -14,9 +14,18 @@ function Chart(data, container) {
   var ctx = canvas.getContext("2d");
   container.appendChild(canvas);
 
+  var overlay = document.createElement("canvas");
+  overlay.className = "overlay";
+  container.appendChild(overlay);
+  overlay.width = canvas.width;
+  overlay.height = canvas.height;
+  var overlayCtx = overlay.getContext("2d");
+
   this.container = container;
   this.canvas = canvas;
+  this.overlay = overlay;
   this.ctx = ctx;
+  this.overlayCtx = overlayCtx;
   this.data = data;
 
   var settings = {};
@@ -42,15 +51,93 @@ function Chart(data, container) {
     y1: 0,
     width: canvas.width,
     height: canvas.height - 2 * settings.preview.height,
-    lineWidth: 2,
+    lineWidth: 3,
     labels: 5
   };
   this.settings = settings;
 
   this.drawLegend();
   this.drawChart();
+  this.bindMouseEvents();
 }
 
+// Bind mouse events
+Chart.prototype.bindMouseEvents = function () {
+  var self = this;
+  var currentIndex;
+  var tooltip = document.createElement("div");
+  tooltip.className = "chart-tooltip";
+  tooltip.style.display = "none";
+  this.container.appendChild(tooltip);
+  this.overlay.addEventListener("mousemove", function(e) {
+    var rect = e.target.getBoundingClientRect();
+    var x = Math.round(e.clientX - rect.left);
+    var y = Math.round(e.clientY - rect.top);
+    if (
+      self.settings.view.x0 <= x && x <= self.settings.view.x1 &&
+      self.settings.view.y1 <= y && y <= self.settings.view.y0
+    ) {
+      var viewPoint = applyTransform(x, y, self.settings.view.transform, true);
+      var pointIndex = binarySearch(self.data.columns[0], viewPoint[0], function (a, b) { return a - b; });
+      if (pointIndex < 0) {
+        pointIndex = Math.abs(pointIndex) - 1;
+      }
+      if (pointIndex === currentIndex) { return; }
+      currentIndex = pointIndex;
+      renderVRule();
+      renderTooltip();
+    }
+  });
+
+  function renderTooltip() {
+    tooltip.innerHTML = "";
+    tooltip.style.display = "block";
+    var xValue = self.data.columns[0][currentIndex];
+    var xCaption = document.createElement("div");
+    xCaption.textContent = new Date(xValue).toDateString();
+    tooltip.appendChild(xCaption);
+    self.data.columns.forEach(function (column) {
+      var columnId = column[0];
+      if (self.settings.displayed.indexOf(columnId) >= 0) {
+        var item = document.createElement("div");
+        item.className = "item";
+        item.style.color = self.data.colors[columnId];
+        tooltip.appendChild(item);
+        var value = document.createElement("div");
+        value.className = "value";
+        item.appendChild(value);
+        var label = document.createElement("div");
+        label.className = "label";
+        item.appendChild(label);
+        var yValue = column[currentIndex];
+        value.textContent = yValue;
+        var yLabel = self.data.names[columnId];
+        label.textContent = yLabel;
+      }
+    });
+  }
+
+  function renderVRule() {
+    var overlayCtx = self.overlayCtx;
+    overlayCtx.save();
+    overlayCtx.clearRect(0, 0, overlayCtx.canvas.width, overlayCtx.canvas.height);
+    overlayCtx.strokeStyle = "#aaa";
+    overlayCtx.save();
+    var transform = self.settings.view.transform;
+    overlayCtx.setTransform(transform.xRatio, 0, 0, transform.yRatio, transform.xOffset, transform.yOffset);
+    overlayCtx.beginPath();
+    var x = self.data.columns[0][currentIndex];
+    var y0 = self.settings.view.transform.minY;
+    var y1 = self.settings.view.transform.maxY;
+    overlayCtx.moveTo(x, y0);
+    overlayCtx.lineTo(x, y1);
+    overlayCtx.restore();
+    overlayCtx.stroke();
+    overlayCtx.restore();
+  }
+};
+
+// Draw chart with animation effect
 Chart.prototype.drawChart = function () {
 
   var self = this;
@@ -107,7 +194,7 @@ Chart.prototype.calcTransformDelta = function (actual, former) {
   return delta;
 };
 
-// Calculate extremes for given data range
+// Draw legend with column controls
 Chart.prototype.drawLegend = function () {
   var self = this;
   var legend = document.createElement("div");
@@ -143,7 +230,7 @@ Chart.prototype.drawLegend = function () {
   });
 };
 
-// Calculate extremes for given data range
+// Calculate extremes and transform params for given data range and view
 Chart.prototype.calcTransform = function (view, begin, end) {
   if (this.settings.displayed.length == 0) { return; }
   var i,
@@ -190,6 +277,7 @@ Chart.prototype.clear = function () {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
 
+// Render view / preview
 Chart.prototype.renderView = function (view, transform) {
   var ctx = this.ctx;
   this.drawLabels(view, transform);
@@ -220,10 +308,11 @@ Chart.prototype.renderView = function (view, transform) {
   ctx.restore();
 };
 
+// Draw labels in view
 Chart.prototype.drawLabels = function (view, transform) {
-  var ctx = this.ctx;
+  if (!view.labels) { return; }
 
-  // Draw labels
+  var ctx = this.ctx;
   ctx.save();
   ctx.font = "12px sans-serif";
   ctx.textBaseline = "bottom";
@@ -256,9 +345,37 @@ Chart.prototype.drawLabels = function (view, transform) {
   ctx.restore();
 };
 
-function applyTransform(x, y, transform) {
-  return [
-    x * transform.xRatio + transform.xOffset,
-    y * transform.yRatio + transform.yOffset
-  ];
+// Transform helper
+function applyTransform(x, y, transform, reverse) {
+  var result;
+  if (reverse) {
+    result = [
+      x / transform.xRatio - transform.xOffset / transform.xRatio,
+      y / transform.yRatio - transform.yOffset / transform.yRatio
+    ];
+  } else {
+    result = [
+      x * transform.xRatio + transform.xOffset,
+      y * transform.yRatio + transform.yOffset
+    ];
+  }
+  return result;
+}
+
+// Binary search helper
+function binarySearch(arr, el, fn) {
+  var m = 0;
+  var n = arr.length - 1;
+  while (m <= n) {
+    var k = (n + m) >> 1;
+    var cmp = fn(el, arr[k]);
+    if (cmp > 0) {
+      m = k + 1;
+    } else if(cmp < 0) {
+      n = k - 1;
+    } else {
+      return k;
+    }
+  }
+  return -m - 1;
 }
